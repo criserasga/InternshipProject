0965e85187e48a2d94eb2195e722c9024acd099a
Cris Erasga <mcerasga@Criss-MacBook-Air.local>
2019-11-26 10:19:45 -0700

    reworked funcitons

--- project/drive.py
+++ project/drive.py
@@ -8,23 +8,48 @@
 # - create new files in Drive without having to keep them on local storage
 
 # 
+# LOCAL VARIABLES:
+# 
+folder_id = ''
+mimeType = None
+file_queue = []
+
+# 
 # NAME:     createFolder
 # PURPOSE:  Creates a Google Drive folder from user's input
 # 
 def createFolder(DRIVE):
-    file_metadata = {
-    'name': eventName,
-    'mimeType': 'application/vnd.google-apps.folder'
+    folder_metadata = {
+        'name': main.eventName,
+        'mimeType': 'application/vnd.google-apps.folder'
     }
-    file = DRIVE.files().create(body=file_metadata, fields='id').execute()
-    folderId = file.get('id')
+    file = DRIVE.files().create(body=folder_metadata, fields='id').execute()
+    folder_id = file.get('id')
 
 # 
-# NAME:     createFolder
-# PURPOSE:  Creates files from user's input
+# NAME:     queueFiles
+# PURPOSE:  Adds files to an array queue to be uploaded
 # 
-def createFiles(DRIVE):
-    file_metadata = {
-        'name': eventName,
-        'parents': folderId
-    }
+def queueFiles(DRIVE, docType):
+    temp_list = []
+    temp_str = main.eventName + ' ' + docType
+    temp_list.append(temp_str)
+    temp_list.append(mimeType)
+    file_metadata = tuple(temp_list)
+    file_queue.append(file_metadata)
+
+# 
+# NAME:     uploadFiles
+# PURPOSE:  Uploads files into their parent folder
+# 
+def moveFiles():
+    for filename, mimeType in file_queue:
+    metadata = {
+        'name': filename,
+        'parents': [folder_id]
+        }
+    if mimeType:
+        metadata['mimeType'] = mimeType
+    result = DRIVE.files().create(body=metadata, media_body=filename).execute()
+    if result:
+        print('Uploaded "%s" (%s)' % (filename, result['mimeType']))
